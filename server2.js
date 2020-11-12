014fd2cec7ee0eb6363acf110a441f7f6b585109
aurel91000 <56591961+aurel91000@users.noreply.github.com>
2020-11-12 12:17:13 +0100

    mutations and course

--- server2.js
+++ server2.js
@@ -3,20 +3,20 @@ const { graphqlHTTP } = require('express-graphql')
 var { buildSchema } = require('graphql');
 
 // GraphQL schema
-var schema = buildSchema(`
-    type Query {
-        course(id: Int!): Course
-        courses(topic: String): [Course]
-    },
-    type Course {
-        id: Int
-        title: String
-        author: String
-        description: String
-        topic: String
-        url: String
-    }
-`);
+// var schema = buildSchema(`
+//     type Query {
+//         course(id: Int!): Course
+//         courses(topic: String): [Course]
+//     },
+//     type Course {
+//         id: Int
+//         title: String
+//         author: String
+//         description: String
+//         topic: String
+//         url: String
+//     }
+// `);
 
 var coursesData = [
     {
@@ -75,4 +75,40 @@ app.use('/graphql',
     graphiql: true
 }),
 );
+
+// GraphQL schema
+var schema = buildSchema(`
+    type Query {
+        course(id: Int!): Course
+        courses(topic: String): [Course]
+    },
+    type Mutation {
+        updateCourseTopic(id: Int!, topic: String!): Course
+    }
+    type Course {
+        id: Int
+        title: String
+        author: String
+        description: String
+        topic: String
+        url: String
+    }
+`);
+
+var updateCourseTopic = function({id, topic}) {
+    coursesData.map(course => {
+        if (course.id === id) {
+            course.topic = topic;
+            return course;
+        }
+    });
+    return coursesData.filter(course => course.id === id) [0];
+}
+
+var root = {
+    course: getCourse,
+    courses: getCourses,
+    updateCourseTopic: updateCourseTopic
+};
+
 app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
